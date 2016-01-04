e04e0aaae1d2043ca56699c373c52257121288d3
Evegenn <demerbrise@gmail.com>
2016-01-04 19:31:01 +0300

    reaorganize code from BlueJ to NetBeansIDE

---
+++ Earthquakes_Programming and Interfaces/src/InterfaceAbstractStarterProgram/IMarkovModel.java
@@ -0,0 +1,16 @@
+package InterfaceAbstractStarterProgram;
+
+
+/**
+ * Write a description of interface IMarkovModel here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public interface IMarkovModel {
+    public void setTraining(String text);
+    
+    public String getRandomText(int numChars);
+    
+}
